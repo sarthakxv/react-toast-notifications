1e61301c990c800a0ea2eba85f8ea340317fcd32
Sarthak Verma <sarthakvdev@gmail.com>
2024-03-22 02:26:01 +0530

    Reducer configured with Context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,46 @@
+"use client";
 import clsx from "clsx";
+import ToastContextProvider from "../store/ToastContextProvider";
 
 export default function Home() {
   const buttonStyle =
     "px-4 py-2 rounded hover:shadow-lg active:shadow-none transition-all duration-300 text-white max-w-md w-full";
 
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-5 sm:p-10">
-      <div className="flex flex-col items-center justify-center space-y-4 p-5 border border-gray-200 shadow-lg rounded-xl">
-        <h1 className="text-3xl font-bold">Add Notifications</h1>
-        <button
-          className={clsx(buttonStyle, "bg-blue-500")}
-          // onClick={handleNotification}
-          data-type="INFO"
-        >
-          Info
-        </button>
-        <button
-          className={clsx(buttonStyle, "bg-green-500")}
-          // onClick={handleNotification}
-          data-type="SUCCESS"
-        >
-          Success
-        </button>
-        <button
-          className={clsx(buttonStyle, "bg-red-500")}
-          // onClick={handleNotification}
-          data-type="ERROR"
-        >
-          Error
-        </button>
-        <button
-          className={clsx(buttonStyle, "bg-yellow-500")}
-          // onClick={handleNotification}
-          data-type="WARNING"
-        >
-          Warning
-        </button>
-      </div>
-    </main>
+    <ToastContextProvider>
+      <main className="flex flex-col items-center justify-center min-h-screen p-5 sm:p-10">
+        <div className="flex flex-col items-center justify-center space-y-4 p-5 border border-gray-200 shadow-lg rounded-xl">
+          <h1 className="text-3xl font-bold">Add Notifications</h1>
+          <button
+            className={clsx(buttonStyle, "bg-blue-500")}
+            // onClick={handleNotification}
+            data-type="INFO"
+          >
+            Info
+          </button>
+          <button
+            className={clsx(buttonStyle, "bg-green-500")}
+            // onClick={handleNotification}
+            data-type="SUCCESS"
+          >
+            Success
+          </button>
+          <button
+            className={clsx(buttonStyle, "bg-red-500")}
+            // onClick={handleNotification}
+            data-type="ERROR"
+          >
+            Error
+          </button>
+          <button
+            className={clsx(buttonStyle, "bg-yellow-500")}
+            // onClick={handleNotification}
+            data-type="WARNING"
+          >
+            Warning
+          </button>
+        </div>
+      </main>
+    </ToastContextProvider>
   );
 }
--- src/store/ToastContextProvider.tsx
+++ src/store/ToastContextProvider.tsx
@@ -1,33 +1,67 @@
 import { useState, useReducer, createContext } from "react";
 import { ToastType } from "../components/Notifications/Toast";
+import { toastReducer } from "./toastReducer";
+import { ToastContainer } from "../components/Notifications";
 
 export const ToastContext = createContext({
-  toasts: {},
-  addToast: (id: number, type: string, message: string) => {},
-  removeToast: (id: number) => {},
+  success: (message: string) => {},
+  warning: (message: string) => {},
+  info: (message: string) => {},
+  error: (message: string) => {},
 });
 
 const ToastContextProvider = ({
   children,
 }: Readonly<React.PropsWithChildren>) => {
-  const [notifications, setNotifications] = useState({
-    toasts: new Array<ToastType>()
-  });
+  const initialState = {
+    toasts: new Array<ToastType>(),
+  };
+
+  // useReducer hook to manage `toasts` state
+  const [state, toastsDispatch] = useReducer(toastReducer, initialState);
 
   // Adds a new toast to the context
-  const addToast = (id: number, type: string, message: string) => {};
+  const addToast = (type: string, message: string) => {
+    const id = Math.floor(Math.random() * 10000000);
+    // `action` object is passed to dispath()
+    toastsDispatch({
+      type: "ADD_TOAST",
+      payload: {
+        id,
+        type,
+        message,
+      },
+    });
+  };
 
   // Removes toast of given id from the context
-  const removeToast = (id: number) => {};
+  const deleteToast = (id: number) => {
+    toastsDispatch({
+      type: "DELETE_TOAST",
+      payload: { id },
+    });
+  };
+
+  // Add methods for each type of Toast
+  const success = (message: string) => {
+    addToast("success", message);
+  };
+
+  const warning = (message: string) => {
+    addToast("warning", message);
+  };
+
+  const info = (message: string) => {
+    addToast("info", message);
+  };
+
+  const error = (message: string) => {
+    addToast("error", message);
+  };
 
   return (
-    <ToastContext.Provider
-      value={{
-        toasts: notifications,
-        addToast,
-        removeToast,
-      }}
-    >
+    <ToastContext.Provider value={{ success, warning, info, error }}>
+      <ToastContainer toasts={state.toasts} />
       {children}
     </ToastContext.Provider>
   );
